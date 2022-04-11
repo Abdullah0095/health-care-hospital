a4966095e44d26c13b26e4e139ff9370467cd971
Abdullah0095 <abdullah.web1995@gmail.com>
2022-04-11 12:27:06 +0600

    services is adding

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Navbar from './Navbar/Navbar';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <Banner/>
+            <Services/>
 
         </div>
     );
---
+++ src/Components/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div style={{marginTop: '50px'}}>
+            <h1>Services are under construction</h1>
+        </div>
+    );
+};
+
+export default Services;
